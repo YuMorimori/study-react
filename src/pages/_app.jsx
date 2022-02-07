65478cba448aa1b60c862d3d79dac165f761a8cd
Yusei Morimoto <yusei0509@icloud.com>
2022-02-07 14:26:00 +0900

    ♻️ appの修正

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,12 +2,13 @@ import 'src/styles/globals.css';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  <>
-    <Head>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Component {...pageProps} />
-  </>
-}
+  return(
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )}
 
 export default MyApp
