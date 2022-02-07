203e9e574cbd08de20bfb6e11b29015f902bc1cb
Yusei Morimoto <yusei0509@icloud.com>
2022-02-07 15:16:44 +0900

    ⚗️ 不要な記述でのちに削除しますが、試しにクリックイベントを書いてみました。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,25 @@ import styles from 'src/styles/Home.module.css';
 import { Footer } from 'src/components/Footer';
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
+import { useCallback } from 'react';
+
+
+
+// メソッドをコンポーネント内部に書くと再レンダリングされるときに
+// 描画されてしまうのでパフォーマンスが落ちる
 
 export default function Home() {
+
+  const foo = 1;
+
+  // useCallbackを使うと再レンダリングされるときに再生成されなくなる
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);//targetを指定すると属性にアクセスできる
+    e.preventDefault();
+    alert(foo)
+  }, []);
+
+  
   /* ファイルシステムルーティングを機能させるためにpages配下の
     ディレクトリにはdefaultをつけておく決まりがある*/
   return (
@@ -13,6 +30,10 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
+      
+      <a href='/about' onClick={handleClick}> {/* //ここに直接処理を書くのはよろしくない */}
+        ボタン
+      </a>
 
       <Main page="index" />
 
