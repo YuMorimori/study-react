efdffdc89b7d5ae346cf78b372b60b95f19c5fe5
Yusei Morimoto <yusei0509@icloud.com>
2022-02-20 15:15:39 +0900

    ♻️ 状態を変更するときにprevをつけて名前をわかりやすくした。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -19,7 +19,7 @@ export default function Home() {
   const handleClick = useCallback(
     () => {
       if (count < 10) {
-        setCount((count) => count + 1);
+        setCount((prevCount) => prevCount + 1);
       }
       // 前の状態を用いて更新したい時はsetの引数に関数を書く
     },
@@ -29,7 +29,7 @@ export default function Home() {
   );
 
   const handleDisplay = useCallback(() => {
-    setIsShow((isShow) => !isShow);
+    setIsShow((prevIsShow) => !prevIsShow);
     // return isShow ? false : true;
   }, []);
 
