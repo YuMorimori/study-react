e1867fe6037b1f805b8faee8a0700d6971fd9ac1
Yusei Morimoto <yusei0509@icloud.com>
2022-03-19 23:31:28 +0900

    🔥 indexから不要なコードを削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,38 +1,21 @@
 import React from "react";
 import styles from "src/styles/Home.module.css";
 import Head from "next/head";
-import { Footer } from "src/components/Footer";
-import { Header } from "src/components/Header";
-import { Main } from "src/components/Main";
 
- const About = (props) => {
+import { Header } from "src/components/Header";
+
+
+const Index = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>About Page</title>
+        <title>Index Page</title>
       </Head>
       <Header />
-      {props.isShow ? <h2>{props.doubleCount}</h2> : null};
-      {/* nullを返すと何も表示させないことが可能になる */}
-      <button href="/about" onClick={props.handleClick}>
-        {" "}
-        {/* //ここに直接処理を書くのはよろしくない */}
-        ボタン
-      </button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>追加</button>
-      <ul>
-        {props.array.map((item) => {
-          return <li key={props.item}>{props.item}</li>;
-        })}
-      </ul>
-      <Main page="about" />
-      <Footer />
+      <h1>Next.jsで学ぶReact講座</h1>
+      <p>JsonPlaceholderのAPIを叩いてみるよ</p>
     </div>
   );
-}
+};
 
-export default About;
+export default Index;
