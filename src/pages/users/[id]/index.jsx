b71b953a0d3ce291fafa272d9db8bd44d3091363
Yusei Morimoto <yusei0509@icloud.com>
2022-04-14 15:10:42 +0900

    ✨ ユーザー詳細ページでSSRを実践

--- src/pages/users/[id]/index.jsx
+++ src/pages/users/[id]/index.jsx
@@ -1,15 +1,31 @@
 // /* eslint-disable @next/next/no-html-link-for-pages */
 import { Header } from "src/components/Header";
 import { UserComponent } from "src/components/User";
+import { SWRConfig } from "swr";
 
-// メソッドをコンポーネント内部に書くと再レンダリングされるときに
-// 描画されてしまうのでパフォーマンスが落ちる
+export const getServerSideProps = async (ctx) => {
+  const { id } = ctx.query;
+  const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+  const user = await fetch(API_URL);
+  const userData = await user.json();
 
-const UsersId = () => {
+  return {
+    props: {
+      fallback: {
+        [API_URL]: userData,
+      },
+    },
+  };
+};
+
+const UsersId = (props) => {
+  const { fallback } = props;
   return (
     <div>
-      <Header />
-      <UserComponent />
+      <SWRConfig value={{ fallback }}>
+        <Header />
+        <UserComponent />
+      </SWRConfig>
     </div>
   );
 };
